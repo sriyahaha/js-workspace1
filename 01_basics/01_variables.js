32531bf8819b6ae73a01594b4e53b694e650ba4d
sriyahaha <163875801+sriyahaha@users.noreply.github.com>
2024-09-22 06:13:33 +0000

    part 1 done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountID = 1454453;
+let accountEmail = "sriyap2804@gmail.com";
+var accountPassword = "12345";
+accountCity = "Pune";
+let accountState;
+// accountID = 2
+accountEmail = "hc@hc.com";
+accountPassword = "203040";
+accountCity = "bangalore";
+ 
+console.table([accountID, accountEmail, accountCity, accountState]);
+console.log(accountID)
